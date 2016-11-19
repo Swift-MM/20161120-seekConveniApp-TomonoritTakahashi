8116a3e59ca6c266bc4bce88a27f825feefee26a
Tomonori <tomonori0204.takahashi@gmail.com>
2016-11-20 03:27:08 +0900

    got current coordinate

--- seekConveniApp/ViewController.swift
+++ seekConveniApp/ViewController.swift
@@ -1,17 +1,8 @@
-//
-//  ViewController.swift
-//  seekConveniApp
-//
-//  Created by 高橋知憲 on 2016/11/20.
-//  Copyright © 2016年 高橋知憲. All rights reserved.
-//
 import UIKit
-import MapKit
 import CoreLocation
 
-
 // CLLocationManagerDelegateを継承しなければならない
-class ViewController: UIViewController,CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     // 現在地の位置情報の取得にはCLLocationManagerを使用
     var lm: CLLocationManager!
@@ -20,18 +11,12 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
     // 取得した経度を保持するインスタンス
     var longitude: CLLocationDegrees!
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // フィールドの初期化
-        
-        //現在地
         lm = CLLocationManager()
-        //軽度
         longitude = CLLocationDegrees()
-        //緯度
         latitude = CLLocationDegrees()
         
         // CLLocationManagerをDelegateに指定
@@ -40,29 +25,28 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
         // 位置情報取得の許可を求めるメッセージの表示．必須．
         lm.requestAlwaysAuthorization()
         // 位置情報の精度を指定．任意，
-        lm.desiredAccuracy = kCLLocationAccuracyBest
+        // lm.desiredAccuracy = kCLLocationAccuracyBest
         // 位置情報取得間隔を指定．指定した値（メートル）移動したら位置情報を更新する．任意．
-        lm.distanceFilter = 10
+        // lm.distanceFilter = 1000
         
         // GPSの使用を開始する
         lm.startUpdatingLocation()
-        
-        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        
     }
     
     /* 位置情報取得成功時に実行される関数 */
-    private func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation){
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation = locations.last
         // 取得した緯度がnewLocation.coordinate.longitudeに格納されている
-        latitude = newLocation.coordinate.latitude
+        latitude = newLocation!.coordinate.latitude
         // 取得した経度がnewLocation.coordinate.longitudeに格納されている
-        longitude = newLocation.coordinate.longitude
+        longitude = newLocation!.coordinate.longitude
+        
         // 取得した緯度・経度をLogに表示
-        NSLog("latiitude: \(latitude) , longitude: \(longitude)")
+        NSLog("latitude: \(latitude) , longitude: \(longitude)")
         
         // GPSの使用を停止する．停止しない限りGPSは実行され，指定間隔で更新され続ける．
         // lm.stopUpdatingLocation()
@@ -73,5 +57,5 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
         // この例ではLogにErrorと表示するだけ．
         NSLog("Error")
     }
+    
 }
-
