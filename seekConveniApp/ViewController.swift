d62866be98ccf1265f748eebe38604ed682f8553
Tomonori <tomonori0204.takahashi@gmail.com>
2016-11-20 12:57:48 +0900

    起動時に現在地の周辺地図を表示

--- seekConveniApp/ViewController.swift
+++ seekConveniApp/ViewController.swift
@@ -3,13 +3,13 @@ import MapKit
 import CoreLocation
 
 // CLLocationManagerDelegateを継承しなければならない
-class ViewController: UIViewController, UISearchBarDelegate,CLLocationManagerDelegate,MKMapViewDelegate {
+class ViewController: UIViewController, UITextFieldDelegate,CLLocationManagerDelegate,MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var destSearchBar: UISearchBar!
     
+//    var annotation = MKPointAnnotation()
     
-    var testManager:CLLocationManager = CLLocationManager()
     
     // 現在地の位置情報の取得にはCLLocationManagerを使用
     var lm: CLLocationManager!
@@ -18,9 +18,13 @@ class ViewController: UIViewController, UISearchBarDelegate,CLLocationManagerDel
     // 取得した経度を保持するインスタンス
     var longitude: CLLocationDegrees!
     
-
     
-    //最初からあるメソッド
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,17 +32,6 @@ class ViewController: UIViewController, UISearchBarDelegate,CLLocationManagerDel
         lm = CLLocationManager()
         longitude = CLLocationDegrees()
         latitude = CLLocationDegrees()
-        
-        
-        //中心座標
-        let center = CLLocationCoordinate2DMake(35.0, 140.0)
-        
-        //表示範囲
-        let span = MKCoordinateSpanMake(1.0, 1.0)
-        
-        //中心座標と表示範囲をマップに登録する。
-        let region = MKCoordinateRegionMake(center, span)
-        mapView.setRegion(region, animated:true)
         
         //デリゲート先に自分を設定する。
         mapView.delegate = self
@@ -63,47 +56,21 @@ class ViewController: UIViewController, UISearchBarDelegate,CLLocationManagerDel
         lm.distanceFilter = 100.0
         
         
+        //中心座標
+        //let center = CLLocationCoordinate2DMake(.coordinate.latitude, .coordinate.longitude) //現在地の座標を入れたい
         
-        //デリゲート先を自分に設定する。
-        searchBar.delegate = self
+        //表示範囲
+        //let span = MKCoordinateSpanMake(10.0, 10.0)
+        
+        //中心座標と表示範囲をマップに登録する。
+        //let region = MKCoordinateRegionMake(center, span)
+        //mapView.setRegion(region, animated:true)
+        
+        
+        
     }
     
     
-    
-    //検索ボタン押下時の呼び出しメソッド
-    func searchButtonClicked(searchBar: UISearchBar) {
-        
-        //キーボードを閉じる。
-        searchBar.resignFirstResponder()
-        
-        //検索条件を作成する。
-        let request = MKLocalSearchRequest()
-        request.naturalLanguageQuery = searchBar.text
-        
-        //検索範囲はマップビューと同じにする。
-        request.region = mapView.region
-        
-        //ローカル検索を実行する。
-        let localSearch:MKLocalSearch = MKLocalSearch(request: request)
-        localSearch.start(completionHandler: {(result, error) in
-            
-            for placemark in (result?.mapItems)! {
-                if(error == nil) {
-                    
-                    //検索された場所にピンを刺す。
-                    let annotation = MKPointAnnotation()
-                    annotation.coordinate = CLLocationCoordinate2DMake(placemark.placemark.coordinate.latitude, placemark.placemark.coordinate.longitude)
-                    annotation.title = placemark.placemark.name
-                    annotation.subtitle = placemark.placemark.title
-                    self.mapView.addAnnotation(annotation)
-                    
-                } else {
-                    //エラー
-                    print(error)
-                }
-            }
-        })
-    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -119,21 +86,28 @@ class ViewController: UIViewController, UISearchBarDelegate,CLLocationManagerDel
         latitude = newLocation!.coordinate.latitude
         // 取得した経度がnewLocation.coordinate.longitudeに格納されている
         longitude = newLocation!.coordinate.longitude
-    
+        
+        //現在位置をマップの中心にして登録する。
+        for location in locations {
+        let center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegionMake(center, span)
+        mapView.setRegion(region, animated:true)
+        }
+        
+        
+        
         // 取得した緯度・経度をLogに表示
         NSLog("latitude: \(latitude) , longitude: \(longitude)")
         // GPSの使用を停止する．停止しない限りGPSは実行され，指定間隔で更新され続ける．
         // lm.stopUpdatingLocation()
     }
-
-
-
-
-
+    
     /* 位置情報取得失敗時に実行される関数 */
-    private func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
-    // この例ではLogにErrorと表示するだけ．
-    NSLog("Error")
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        // この例ではLogにErrorと表示するだけ．
+        NSLog("Error")
     }
-
+    
+    
 }
